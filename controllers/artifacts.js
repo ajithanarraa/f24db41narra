fa92c2484733e592e7364137df9bcb5ed3ef192a
Ajitha Narraa <ajithanarra23@gmail.com>
2024-11-21 04:05:19 -0600

    Assignment 12

--- controllers/artifacts.js
+++ controllers/artifacts.js
@@ -1,150 +1,85 @@
-// controllers/artifactsController.js
 const Artifact = require('../models/artifacts');
-
-// // List all artifacts (already implemented)
-// exports.artifact_list = async function(req, res) {
-//   try {
-//     const artifacts = await Artifact.find();
-//     res.json(artifacts);
-//   } catch (err) {
-//     res.status(500).send({ error: 'Failed to fetch artifacts' });
-//   }
-// };
-
 // List of all artifacts
-exports.artifact_list = async function(req, res) {
- try{
- theartifacts = await Artifact.find();
- res.send(theartifacts);
- }
- catch(err){
- res.status(500);
- res.send(`{"error": ${err}}`);
- } 
+exports.artifact_list = async function (req, res) {
+  try {
+    theartifacts = await Artifact.find();
+    res.send(theartifacts);
+  }
+  catch (err) {
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+  }
 };
 
 // VIEWS
 // Handle a show all view
-exports.artifact_view_all_Page = async function(req, res) {
- try{
- theartifacts = await Artifact.find();
- res.render('artifacts', { title: 'artifact Search Results', results: theartifacts });
- }
- catch(err){
- res.status(500);
- res.send(`{"error": ${err}}`);
- } 
+exports.artifact_view_all_Page = async function (req, res) {
+  try {
+    theartifacts = await Artifact.find();
+    res.render('artifacts', { title: 'artifact Search Results', results: theartifacts });
+  }
+  catch (err) {
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+  }
 };
 
 // Handle artifact create on POST.
-exports.artifact_create_post = async function(req, res) {
- console.log(req.body)
- let document = new Artifact();
- // We are looking for a body, since POST does not have query parameters.
- // Even though bodies can be in many different formats, we will be picky
- // and require that it be a json object
- // {"artifact_type":"goat", "cost":12, "size":"large"}
- document.artifact_type = req.body.artifact_type;
- document.origin = req.body.origin;
- document.age = req.body.age;
- try{
- let result = await document.save();
- res.send(result);
- }
- catch(err){
- res.status(500);
- res.send(`{"error": ${err}}`);
- } 
+exports.artifact_create_post = async function (req, res) {
+  console.log(req.body)
+  let document = new Artifact();
+  // We are looking for a body, since POST does not have query parameters.
+  // Even though bodies can be in many different formats, we will be picky
+  // and require that it be a json object
+  // {"artifact_type":"goat", "cost":12, "size":"large"}
+  document.artifact_type = req.body.artifact_type;
+  document.origin = req.body.origin;
+  document.age = req.body.age;
+  try {
+    let result = await document.save();
+    res.send(result);
+  }
+  catch (err) {
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+  }
 };
-
-
-// // Create a new artifact (already implemented)
-// exports.artifact_create_post = async function(req, res) {
-//   try {
-//     const { artifact_type, origin, age } = req.body;
-//     if (!artifact_type || !origin || !age) {
-//       return res.status(400).json({ message: "All fields (artifact_type, origin, age) are required" });
-//     }
-//     const newArtifact = new Artifact({ artifact_type, origin, age });
-//     const savedArtifact = await newArtifact.save();
-//     res.status(201).json(savedArtifact);
-//   } catch (err) {
-//     res.status(500).json({ message: `Error: ${err.message}` });
-//   }
-// };
-
-
 // for a specific artifact.
-exports.artifact_detail = async function(req, res) {
+exports.artifact_detail = async function (req, res) {
   console.log("detail" + req.params.id)
   try {
-  result = await Artifact.findById( req.params.id)
-  res.send(result)
+    result = await Artifact.findById(req.params.id)
+    res.send(result)
   } catch (error) {
-  res.status(500)
-  res.send(`{"error": document for id ${req.params.id} not found`);
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
   }
- };
-
-
-
-// // Get details of a specific artifact by ID
-// exports.artifact_detail = async function(req, res) {
-//   try {
-//     const artifact = await Artifact.findById(req.params.id);
-//     if (!artifact) {
-//       return res.status(404).json({ message: "Artifact not found" });
-//     }
-//     res.status(200).json(artifact);
-//   } catch (err) {
-//     res.status(500).json({ message: `Error: ${err.message}` });
-//   }
-// };
-
+};
 
 //Handle artifact update form on PUT.
-exports.artifact_update_put = async function(req, res) {
- console.log(`update on id ${req.params.id} with body 
-${JSON.stringify(req.body)}`)
- try {
- let toUpdate = await artifact.findById( req.params.id)
- // Do updates of properties
- if(req.body.artifact_type) 
- toUpdate.artifact_type = req.body.artifact_type;
- if(req.body.origin) toUpdate.origin = req.body.origin;
- if(req.body.age) toUpdate.age = req.body.age;
- let result = await toUpdate.save();
- console.log("Sucess " + result)
- res.send(result)
- } catch (err) {
- res.status(500)
- res.send(`{"error": ${err}: Update for id ${req.params.id} 
-failed`);
- }
+exports.artifact_update_put = async function (req, res) {
+  console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`);
+  try {
+    let toUpdate = await Artifact.findById(req.params.id);
+    if (!toUpdate) {
+      return res.status(404).send({ error: `Artifact with id ${req.params.id} not found` });
+    }
+    // Update properties
+    if (req.body.artifact_type) toUpdate.artifact_type = req.body.artifact_type;
+    if (req.body.origin) toUpdate.origin = req.body.origin;
+    if (req.body.age) toUpdate.age = req.body.age;
+
+    let result = await toUpdate.save();
+    console.log("Success " + result);
+    res.status(200).json(result); // Send a JSON response on success
+  } catch (err) {
+    res.status(500).send({ error: `${err.message}: Update for id ${req.params.id} failed` });
+  }
 };
 
 
-// // Update an existing artifact by ID
-// exports.artifact_update_put = async function(req, res) {
-//   try {
-//     let artifact = await Artifact.findById(req.params.id);
-//     if (!artifact) {
-//       return res.status(404).json({ message: "Artifact not found" });
-//     }
-
-//     if (req.body.artifact_type) artifact.artifact_type = req.body.artifact_type;
-//     if (req.body.origin) artifact.origin = req.body.origin;
-//     if (req.body.age) artifact.age = req.body.age;
-
-//     const updatedArtifact = await artifact.save();
-//     res.status(200).json(updatedArtifact);
-//   } catch (err) {
-//     res.status(500).json({ message: `Error: ${err.message}` });
-//   }
-// };
-
 // Delete an artifact by ID
-exports.artifact_delete = async function(req, res) {
+exports.artifact_delete = async function (req, res) {
   try {
     const deletedArtifact = await Artifact.findByIdAndDelete(req.params.id);
     if (!deletedArtifact) {
@@ -153,5 +88,60 @@ exports.artifact_delete = async function(req, res) {
     res.status(200).json({ message: "Artifact deleted successfully" });
   } catch (err) {
     res.status(500).json({ message: `Error: ${err.message}` });
+  }
+};
+
+// Handle a show one view with id specified by query
+exports.artifact_view_one_Page = async function (req, res) {
+  console.log("single view for id " + req.query.id)
+  try {
+    result = await Artifact.findById(req.query.id)
+    res.render('artifactdetail',
+      { title: 'Artifact Detail', toShow: result });
+  }
+  catch (err) {
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+  }
+};
+
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.artifact_create_Page = function (req, res) {
+  console.log("create view")
+  try {
+    res.render('artifactcreate', { title: 'Artifact Create' });
+  }
+  catch (err) {
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+  }
+};
+
+// Handle building the view for updating a costume.
+// query provides the id
+exports.artifact_update_Page = async function (req, res) {
+  console.log("update view for item " + req.query.id)
+  try {
+    let result = await Artifact.findById(req.query.id)
+    res.render('artifactupdate', { title: 'Artifact Update', toShow: result });
+  }
+  catch (err) {
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+  }
+};
+
+// Handle a delete one view with id from query
+exports.artifact_delete_Page = async function (req, res) {
+  console.log("Delete view for id " + req.query.id)
+  try {
+    result = await Artifact.findById(req.query.id)
+    res.render('artifactdelete', { title: 'Artifact Delete', toShow: result });
+  }
+  catch (err) {
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
   }
 };
--- routes/artifacts.js
+++ routes/artifacts.js
@@ -3,23 +3,19 @@ var express = require('express');
 var router = express.Router();
 // const artifact_controlers= require('../controllers/artifacts');
 const artifactsController = require('../controllers/artifacts');
-
-// Define the route for fetching all artifacts
-// router.get('/', artifactsController.artifact_list);
-
-// Define the route for creating an artifact
-// router.post('/', artifactsController.artifact_create_post);
-
-// Define the route for fetching a specific artifact by ID
-// router.get('/:id', artifactsController.artifact_detail);
-
-// Define the route for updating an artifact by ID
-// router.put('/:id', artifactsController.artifact_update_put);
-
-// Define the route for deleting an artifact by ID
-// router.delete('/:id', artifactsController.artifact_delete);
 router.get('/', artifactsController.artifact_view_all_Page );
-
 // GET request for one artifact.
 router.get('/artifacts/:id', artifactsController.artifact_detail);
+// Import the artifact controller (make sure the file path is correct)
+const artifact_controller = require('../controllers/artifacts');
+/* GET detail costume page */
+router.get('/detail', artifactsController.artifact_view_one_Page);
+// DELETE request to delete an artifact by ID
+router.delete('/artifacts/:id', artifact_controller.artifact_delete);
+/* GET create costume page */
+router.get('/create', artifact_controller.artifact_create_Page);
+/* GET create update page */
+router.get('/update', artifact_controller.artifact_update_Page);
+/* GET delete costume page */
+router.get('/delete', artifact_controller.artifact_delete_Page)
 module.exports = router;
